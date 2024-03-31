04b330d9ee743a66b8de86f4e9a837d028dd2a62
Cecile HURIAUX <cecile.huriaux@gmail.com>
2024-03-31 23:23:24 +0200

    feat(Service) Définition de l'interface LitService qui déclare des méthodes pour afficher la liste des lits disponibles, afficher un lit précis par l'Id et affecter un patient à un lit disponible

--- Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/business/service/LitService.java
+++ Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/business/service/LitService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import co.simplon.ECF_Appli_Hopital.business.dto.LitDTO;
 
 public interface LitService {
-    LitDTO afficherLitDisponible(LitDTO litDispo);
+    LitDTO affecterPatientLit(LitDTO litDispo);
 
     List<LitDTO> afficherListeLitsDisponibles();
 
