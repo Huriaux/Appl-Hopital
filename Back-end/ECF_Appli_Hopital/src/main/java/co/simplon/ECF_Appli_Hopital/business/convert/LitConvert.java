4be277017766cd3dfb3e7e4347b8a3718cd011bb
Cecile HURIAUX <cecile.huriaux@gmail.com>
2024-03-31 22:29:18 +0200

    feat(Convertisseur) Création des méthodes de convertion des Entités en DTO et inversement, ainsi que pour les listes

--- Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/business/convert/LitConvert.java
+++ Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/business/convert/LitConvert.java
@@ -1,21 +1,54 @@
-// package co.simplon.ECF_Appli_Hopital.business.convert;
+package co.simplon.ECF_Appli_Hopital.business.convert;
 
-// import co.simplon.ECF_Appli_Hopital.business.dto.LitDTO;
-// import co.simplon.ECF_Appli_Hopital.persistence.entity.Lit;
+import java.util.ArrayList;
+import java.util.List;
 
-// public class LitConvert {
-//     private static LitConvert instance;
+import co.simplon.ECF_Appli_Hopital.business.dto.LitDTO;
+import co.simplon.ECF_Appli_Hopital.persistence.entity.Lit;
 
-//     public static LitConvert getInstance() {
-//         if (instance == null) {
-//             instance = new LitConvert();
-//         }
-//         return instance;
-//     }
+public class LitConvert {
+    private static LitConvert instance;
 
-//     // Convert Patient (Entity) en Patient(DTO)
-//     public LitDTO convertLitToDTO(final Lit litEntity) {
-//         LitDTO litDto = new LitDTO();
+    public static LitConvert getInstance() {
+        if (instance == null) {
+            instance = new LitConvert();
+        }
+        return instance;
+    }
 
-//     }
-// }
+    // Convert Patient (Entity) en Patient(DTO)
+    public LitDTO convertLitToDTO(final Lit litEntity) {
+        LitDTO litDto = new LitDTO();
+        litDto.setIdLit(litEntity.getIdLit());
+        litDto.setDispoLit(litEntity.getDispoLit());
+        return litDto;
+    }
+
+    // Convert Patient (DTO) à Patient(Entity)
+    public Lit convertLitToEntity(final LitDTO litDto) {
+        Lit litEntity = new Lit();
+        litEntity.setIdLit(litDto.getIdLit());
+        litEntity.setDispoLit(litDto.getDispoLit());
+        return litEntity;
+    }
+
+    // Listes :
+
+    // Convert liste Patient (Entity) en liste Patient (DTO)
+    public List<LitDTO> convertListeLitToEntity(final List<Lit> listeLitEntity) {
+        List<LitDTO> listeLitDTO = new ArrayList<>();
+        for (final Lit l : listeLitEntity) {
+            listeLitDTO.add(convertLitToDTO(l));
+        }
+        return listeLitDTO;
+    }
+
+    // Convert liste Patient (DTO) en liste Patient (Entity)
+    public List<Lit> convertListeLitToDTO(final List<LitDTO> listeLitDTO) {
+        List<Lit> listeLitEntity = new ArrayList<>();
+        for (final LitDTO lDto : listeLitDTO) {
+            listeLitEntity.add(convertLitToEntity(lDto));
+        }
+        return listeLitEntity;
+    }
+}
