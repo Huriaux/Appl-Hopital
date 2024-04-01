3f2f989264712da6b9e7038b4b42f9fb529ad9fe
Cecile HURIAUX <cecile.huriaux@gmail.com>
2024-04-01 16:32:50 +0200

    feat(DTO) Création de la classe ChambreDTO

---
+++ Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/business/dto/ChambreDTO.java
@@ -0,0 +1,34 @@
+package co.simplon.ECF_Appli_Hopital.business.dto;
+
+public class ChambreDTO {
+    private long idChambre;
+    private String numChambre;
+    private long idLit; // clé étrangère de la Table Lit
+
+    // getters et setters
+
+    public long getIdChambre() {
+        return idChambre;
+    }
+
+    public void setIdChambre(long idChambre) {
+        this.idChambre = idChambre;
+    }
+
+    public String getNumChambre() {
+        return numChambre;
+    }
+
+    public void setNumChambre(String numChambre) {
+        this.numChambre = numChambre;
+    }
+
+    public long getIdLit() {
+        return idLit;
+    }
+
+    public void setIdLit(long idLit) {
+        this.idLit = idLit;
+    }
+
+}
