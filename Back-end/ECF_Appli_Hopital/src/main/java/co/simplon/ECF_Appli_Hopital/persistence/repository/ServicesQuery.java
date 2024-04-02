a55466a242d1869d42b7e3d462182d62e1dae540
Cecile HURIAUX <cecile.huriaux@gmail.com>
2024-04-02 13:55:43 +0200

    feat(Repository) Création de la classe ServiceQuery

--- Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/persistence/repository/ServicesQuery.java
+++ Back-end/ECF_Appli_Hopital/src/main/java/co/simplon/ECF_Appli_Hopital/persistence/repository/ServicesQuery.java
@@ -1,5 +1,9 @@
 package co.simplon.ECF_Appli_Hopital.persistence.repository;
 
-public class ServicesQuery {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.simplon.ECF_Appli_Hopital.persistence.entity.Services;
+
+public interface ServicesQuery extends JpaRepository<Services, Long> {
 
 }
